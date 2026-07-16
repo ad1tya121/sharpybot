5db5453cd742704993ffaca84a88e5cf5d4d618f
ad1tya121 <adityasarojpatidar@gmail.com>
2026-07-16 13:56:33 +0530

    change the train and model acc to new arch

--- models/model.py
+++ models/model.py
@@ -6,18 +6,14 @@ class NNUE(nn.Module):
         super().__init__()
 
         self.ft = nn.Linear(40960, 256) #feature transformer
-        self.l1 = nn.Linear(512, 32)    # hidden layer 1
+        self.l1 = nn.Linear(256, 32)    # hidden layer 1
         self.l2 = nn.Linear(32, 32)     # hidden layer 2
         self.l3 = nn.Linear(32, 1)      # output
 
-    def forward(self, us_features, them_features):
-        w = torch.clamp(self.ft(us_features), 0, 1)
-        b = torch.clamp(self.ft(them_features), 0, 1)
-
-        features = torch.cat([w, b], dim=1)
-
-        x = torch.clamp(self.l1(features), 0, 1)
-        x = torch.clamp(self.l2(x), 0, 1)
-        
-        return self.l3(x)
+    def forward(self, x):
+        x = torch.clamp(self.ft(x), 0.0, 1.0)
+        x = torch.clamp(self.l1(x), 0.0, 1.0)
+        x = torch.clamp(self.l2(x), 0.0, 1.0)
+        x = self.l3(x)
+        return torch.sigmoid(x)
     
--- models/train.py
+++ models/train.py
@@ -3,41 +3,42 @@ from dataset import ChessDataset
 from torch.utils.data import Dataset, DataLoader
 import torch
 import torch.nn as nn
-import math
 import os
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-loss = nn.BCEWithLogitsLoss()
-
 if __name__ == '__main__':
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    dataset = ChessDataset("lichess-evals.csv")
+    dataloader = DataLoader(dataset, batch_size=2048, shuffle=True, num_workers=2)
+
     model = NNUE().to(device)
-    if os.path.exists('./model.pt'):
-        model.load_state_dict(torch.load('./model.pt', weights_only=True))
+
+    if os.path.exists("model.pt"):
+        model.load_state_dict(torch.load("model.pt", weights_only=True))
         print("Resumed from checkpoint")
+    else:
+        print("Saved Weights not found, creating new model.pt")
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.5)
+    loss = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.00005)
 
 
-    dataset = ChessDataset(r"./lichess-evals")
-    dataloader = DataLoader(dataset=dataset, batch_size=1024, shuffle=True, num_workers=2)
-
-    # training loop
     num_epoch = 40
+    
     for epoch in range(num_epoch):
-        for us_values, them_values, score in dataloader:
-            us_values = us_values.to(device)
-            them_values = them_values.to(device)
+        totalLoss = 0.0
+        for b_idx, (features, score) in enumerate(dataloader):
+            features = features.to(device)
             score = score.to(device)
 
-            #zero gradiants
-            optimizer.zero_grad()
-
             #forward pass
-            score_pred = model(us_values, them_values)
+            score_pred = model(features)
 
             #loss 
             l = loss(score_pred, score)
+
+            #zero gradiants
+            optimizer.zero_grad()
 
             # gradiants
             l.backward()
@@ -45,10 +46,11 @@ if __name__ == '__main__':
             #update weights
             optimizer.step()
             
-            print(f'epoch {epoch+1}, loss {l.item():.4f}')
-
-        scheduler.step()
-        torch.save(model.state_dict(), './model.pt')
-        print("Saved checkpoint")
+            totalLoss += l.item()
+            
+            if b_idx % 10 == 0:
+                print(f"epoch {epoch+1} | batch {b_idx}/{len(dataloader)} | loss: {l.item():.4f}")
+                
+        print(f"epoch {epoch+1} | loss: {totalLoss / len(dataloader):.5f}")
+        torch.save(model.state_dict(), "model.pt")
    
-
